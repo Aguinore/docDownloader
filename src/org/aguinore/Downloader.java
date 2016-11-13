cca40abd0785f03ceb106fdd7b9a3d8cbc2a0293
Aguinore <aguinore@gmail.com>
2016-11-13 09:39:06 +0300

    parse dir

--- src/org/aguinore/Downloader.java
+++ src/org/aguinore/Downloader.java
@@ -1,9 +1,33 @@
 package org.aguinore;
 
+import java.io.File;
+
 public class Downloader {
     private final String pathToFiles;
 
     public Downloader(String pathToFiles) {
         this.pathToFiles = pathToFiles;
+    }
+
+    public void saveDocs() {
+        File baseDir = new File(pathToFiles);
+        if (!baseDir.isDirectory()) {
+            throw new IllegalArgumentException("Provided path must be a directory");
+        }
+
+        File[] files = baseDir.listFiles();
+        if (files == null) {
+            throw new IllegalArgumentException("Provided path must be a nonempty directory");
+        }
+
+        for (File file : files) {
+            if (file.isFile() && file.getName().endsWith(".html")) {
+                parseHtmlFile(file);
+            }
+        }
+    }
+
+    private void parseHtmlFile(File file) {
+        
     }
 }
